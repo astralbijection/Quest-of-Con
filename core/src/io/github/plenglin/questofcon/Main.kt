a656c92ad70a0579e1d7438b7e8176412b4a22a3
Astrid Yu <astrid@astrid.tech>
2017-10-30 16:09:08 -0700

    Add Kotlin

---
+++ core/src/io/github/plenglin/questofcon/Main.kt
@@ -0,0 +1,11 @@
+package io.github.plenglin.questofcon
+
+import com.badlogic.gdx.Screen
+import io.github.plenglin.questofcon.screen.GameScreen
+import ktx.app.KtxGame
+
+/**
+ *
+ */
+class Main : KtxGame<Screen>(GameScreen) {
+}
