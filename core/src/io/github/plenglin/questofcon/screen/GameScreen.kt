033872e8d35722f8eb43556f1ecfe02a55311344
Astrid Yu <astrid@astrid.tech>
2017-10-30 18:14:28 -0700

    Add camera controls

--- core/src/io/github/plenglin/questofcon/screen/GameScreen.kt
+++ core/src/io/github/plenglin/questofcon/screen/GameScreen.kt
@@ -1,6 +1,9 @@
 package io.github.plenglin.questofcon.screen
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.InputMultiplexer
+import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
@@ -8,6 +11,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.badlogic.gdx.utils.viewport.Viewport
+import io.github.plenglin.questofcon.QuestOfCon
 import io.github.plenglin.questofcon.game.Game
 import io.github.plenglin.questofcon.game.render.WorldRenderer
 import ktx.app.KtxScreen
@@ -24,6 +28,7 @@ object GameScreen : KtxScreen {
 
     lateinit var uiStage: Stage
     val uiViewport: Viewport = ScreenViewport()
+    lateinit var mapMovement: MapMovement
 
     lateinit var game: Game
 
@@ -33,8 +38,13 @@ object GameScreen : KtxScreen {
         uiStage.viewport = uiViewport
         shape = ShapeRenderer()
         game = Game()
+        mapMovement = MapMovement(gridCam)
 
         worldRenderer = WorldRenderer(game.world)
+        Gdx.input.inputProcessor = InputMultiplexer(mapMovement)
+
+        gridCam.zoom = 1/32f
+        gridCam.position.set(0f, 0f, 0f)
     }
 
     override fun render(delta: Float) {
@@ -42,8 +52,8 @@ object GameScreen : KtxScreen {
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
         Gdx.gl20.glClearColor(0f, 0f, 0f ,1f)
 
-        gridCam.zoom = 1/32f
-        gridCam.position.set(0f, 0f, 0f)
+        mapMovement.update(delta)
+
         gridCam.update()
 
         worldRenderer.shape.projectionMatrix = gridCam.combined
@@ -60,5 +70,88 @@ object GameScreen : KtxScreen {
     override fun resize(width: Int, height: Int) {
         gridCam.setToOrtho(false, width.toFloat(), height.toFloat())
     }
+
+}
+
+class MapMovement(val cam: OrthographicCamera) : InputProcessor {
+
+    var vx: Int = 0
+    var vy: Int = 0
+    var fast: Boolean = false
+
+    override fun scrolled(amount: Int): Boolean {
+        when (amount) {
+            1 -> cam.zoom *= QuestOfCon.zoomRate
+            -1 -> cam.zoom /= QuestOfCon.zoomRate
+        }
+        return true
+    }
+
+    override fun keyDown(keycode: Int): Boolean {
+        when (keycode) {
+            Input.Keys.SHIFT_LEFT -> {
+                fast = true
+                return false
+            }
+
+            Input.Keys.W, Input.Keys.UP -> {
+                vy = 1
+                return true
+            }
+            Input.Keys.S, Input.Keys.DOWN -> {
+                vy = -1
+                return true
+            }
+
+            Input.Keys.A, Input.Keys.LEFT -> {
+                vx = -1
+                return true
+            }
+            Input.Keys.D, Input.Keys.RIGHT -> {
+                vx = 1
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun keyUp(keycode: Int): Boolean {
+        when (keycode) {
+            Input.Keys.SHIFT_LEFT -> {
+                fast = false
+                return false
+            }
+
+            Input.Keys.W, Input.Keys.S, Input.Keys.UP, Input.Keys.DOWN -> {
+                if (vy != 0) {
+                    vy = 0
+                    return true
+                }
+            }
+
+            Input.Keys.A, Input.Keys.D, Input.Keys.LEFT, Input.Keys.RIGHT -> {
+                if (vx != 0) {
+                    vx = 0
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun update(delta: Float) {
+        val mult = if (fast) 2 else 1
+        cam.translate(vx * mult * QuestOfCon.camSpeed * delta, vy * mult * QuestOfCon.camSpeed * delta)
+    }
+
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean = false
+
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = false
+
+    override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int) = false
+
+    override fun mouseMoved(screenX: Int, screenY: Int) = false
+
+    override fun keyTyped(character: Char) = false
 
 }
