226e1f275613c295fb5cae64b9781213e5d3213a
plenglin <maximumpwnguin@gmail.com>
2017-10-30 17:42:57 -0700

    Grid rendering works

--- core/src/io/github/plenglin/questofcon/game/grid/World.kt
+++ core/src/io/github/plenglin/questofcon/game/grid/World.kt
@@ -1,16 +1,52 @@
 package io.github.plenglin.questofcon.game.grid
 
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+
 /**
  *
  */
-class World(val width: Int, val length: Int) {
+class World(val width: Int, val height: Int) {
 
     val grid = Array(width, {
-        Array(length, { Tile(Terrains.grass) })
+        Array(height, { Tile(Terrains.grass) })
     })
 
     operator fun get(i: Int, j: Int): Tile {
         return grid[i][j]
+    }
+
+    fun draw(shape: ShapeRenderer, drawGrid: Boolean = true) {
+
+        shape.begin(ShapeRenderer.ShapeType.Filled)
+
+        // Draw the terrain
+        grid.forEachIndexed { i, col ->
+            col.forEachIndexed { j, tile ->
+                shape.color = tile.terrain.color
+                shape.rect(i.toFloat(), j.toFloat(), 1f, 1f)
+            }
+        }
+        shape.end()
+
+        // Draw the grid if necessary
+        if (drawGrid) {
+            shape.color = Color(0f, 0.5f, 1f, 0.5f)
+            shape.begin(ShapeRenderer.ShapeType.Line)
+            // Columns
+            for (i in 0..width) {
+                val x = i.toFloat()
+                shape.line(x, 0f, x, height.toFloat())
+            }
+
+            // Rows
+            for (j in 0..height) {
+                val y = j.toFloat()
+                shape.line(0f, y, width.toFloat(), y)
+            }
+            shape.end()
+        }
+
     }
 
 }
--- core/src/io/github/plenglin/questofcon/screen/GameScreen.kt
+++ core/src/io/github/plenglin/questofcon/screen/GameScreen.kt
@@ -1,10 +1,15 @@
 package io.github.plenglin.questofcon.screen
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.badlogic.gdx.utils.viewport.Viewport
+import io.github.plenglin.questofcon.game.Game
 import ktx.app.KtxScreen
 
 /**
@@ -14,22 +19,43 @@ object GameScreen : KtxScreen {
 
     val gridCam = OrthographicCamera()
     lateinit var batch: SpriteBatch
+    lateinit var shape: ShapeRenderer
 
     lateinit var uiStage: Stage
     val uiViewport: Viewport = ScreenViewport()
+
+    lateinit var game: Game
 
     override fun show() {
         batch = SpriteBatch()
         uiStage = Stage()
         uiStage.viewport = uiViewport
+        shape = ShapeRenderer()
+        game = Game()
     }
 
     override fun render(delta: Float) {
 
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+        Gdx.gl20.glClearColor(0f, 0f, 0f ,1f)
+
+        gridCam.zoom = 1/32f
+        gridCam.position.set(0f, 0f, 0f)
+        gridCam.update()
+        shape.projectionMatrix = gridCam.combined
+
+        game.world.draw(shape)
+
     }
 
     override fun dispose() {
+        batch.dispose()
+        shape.dispose()
+        uiStage.dispose()
+    }
 
+    override fun resize(width: Int, height: Int) {
+        gridCam.setToOrtho(false, width.toFloat(), height.toFloat())
     }
 
 }
