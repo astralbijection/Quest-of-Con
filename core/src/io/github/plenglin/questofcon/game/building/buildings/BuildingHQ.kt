9bda776761ce9c7ec75e175594242a5a28a295cf
Astrid Yu <astrid@astrid.tech>
2017-11-01 23:37:00 -0700

    No, you can't demolish the HQ.

--- core/src/io/github/plenglin/questofcon/game/building/buildings/BuildingHQ.kt
+++ core/src/io/github/plenglin/questofcon/game/building/buildings/BuildingHQ.kt
@@ -7,6 +7,7 @@ import io.github.plenglin.questofcon.game.Team
 import io.github.plenglin.questofcon.game.building.Building
 import io.github.plenglin.questofcon.game.building.BuildingCreator
 import io.github.plenglin.questofcon.game.grid.WorldCoords
+import io.github.plenglin.questofcon.ui.Selectable
 import ktx.assets.getValue
 import ktx.assets.load
 
@@ -16,6 +17,10 @@ class BuildingHQ(team: Team, pos: WorldCoords) : Building("HQ", team, pos, 100) 
     override val texture: Texture = Textures.HEADQUARTERS()
 
     override fun getMoneyPerTurn(): Int = 15
+
+    override fun getActions(): List<Selectable> {
+        return emptyList()  // No demolishing the HQ!
+    }
 
     companion object : BuildingCreator("HQ", 0) {
 
