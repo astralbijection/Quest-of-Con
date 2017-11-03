600e1e54c7ab310f28de559a43ace9435b641306
Astrid Yu <astrid@astrid.tech>
2017-11-02 23:42:01 -0700

    Change terrain colors
    For the benefit of your eyes

--- core/src/io/github/plenglin/questofcon/game/grid/Terrain.kt
+++ core/src/io/github/plenglin/questofcon/game/grid/Terrain.kt
@@ -7,16 +7,16 @@ open class Terrain(val name: String, val color: Color, val passable: Boolean, va
 
 object Terrains {
 
-    val grass       = Terrain("grassland", Color.GREEN, true, true)
     val lowlands    = Terrain("lowlands", Color.TAN, true, true)
+    val grass       = Terrain("grassland", Color(0f, 0.7f, 0f, 1f), true, true)
+    val hills       = Terrain("hills", Color(0.3f, 0.5f, 0.1f, 1f), true, true, movementCost = 3)
+    val water       = Terrain("water", Color(0.1f, 0.4f, 0.8f, 1f), false, false, movementCost = 3)
+    val mountains   = Terrain("mountains", Color.BLACK, false, false)
+
     val forest      = Terrain("forest", Color.BROWN, true, false, movementCost = 2)
     val desert      = Terrain("desert", Color.GOLD, true, true, movementCost = 2)
     val gravel      = Terrain("gravel", Color.LIGHT_GRAY, true, true, movementCost = 2)
     val jungle      = Terrain("jungle", Color.BROWN, true, false, movementCost = 3)
-    val hills       = Terrain("hills", Color.DARK_GRAY, true, true, movementCost = 3)
     val swamp       = Terrain("swamp", Color(0f, 0.4f, 0.25f, 1f), true, true, movementCost = 3)
-    val water       = Terrain("water", Color(0.1f, 0.4f, 0.8f, 1f), false, false, movementCost = 3)
-    val mountains   = Terrain("mountains", Color.BLACK, false, false)
-    val snowyPeak   = Terrain("snowy mountains", Color.BLACK, false, false)
 
 }
