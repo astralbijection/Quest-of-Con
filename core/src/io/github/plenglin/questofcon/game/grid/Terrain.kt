2d99f31a514de61b18f4654d0d667832e4be075b
Astrid Yu <astrid@astrid.tech>
2017-11-03 16:17:42 -0700

    Add a FG and BG to textures

--- core/src/io/github/plenglin/questofcon/game/grid/Terrain.kt
+++ core/src/io/github/plenglin/questofcon/game/grid/Terrain.kt
@@ -2,10 +2,11 @@ package io.github.plenglin.questofcon.game.grid
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
+import io.github.plenglin.questofcon.TerrainTextures
 import io.github.plenglin.questofcon.Textures
 
 
-data class Terrain(val name: String, val texture: Textures, val passable: Boolean, val buildable: Boolean, val movementCost: Int = 1) {
+data class Terrain(val name: String, val texture: TerrainTextures, val passable: Boolean, val buildable: Boolean, val movementCost: Int = 1) {
 
     override fun toString(): String {
         return "Terrain($name)"
@@ -15,12 +16,12 @@ data class Terrain(val name: String, val texture: Textures, val passable: Boolea
 
 object Terrains {
 
-    val lowlands    = Terrain("lowlands", Textures.SAND, true, true)
-    val grass       = Terrain("grassland", Textures.GRASS, true, true)
-    val bigHills    = Terrain("big hills", Textures.BIGHILL, true, true, movementCost = 3)
-    val hills       = Terrain("hills", Textures.SMALLHILL, true, true, movementCost = 2)
-    val water       = Terrain("water", Textures.WATER, false, false, movementCost = 3)
-    val mountains   = Terrain("mountains", Textures.MOUNTAIN, false, false)
+    val lowlands    = Terrain("lowlands", TerrainTextures.SAND, true, true)
+    val grass       = Terrain("grassland", TerrainTextures.GRASS, true, true)
+    val bigHills    = Terrain("big hills", TerrainTextures.BIGHILL, true, true, movementCost = 3)
+    val hills       = Terrain("hills", TerrainTextures.SMALLHILL, true, true, movementCost = 2)
+    val water       = Terrain("water", TerrainTextures.WATER, false, false, movementCost = 3)
+    val mountains   = Terrain("mountains", TerrainTextures.MOUNTAIN, false, false)
 
     /*
     val forest      = Terrain("forest", Color.BROWN, true, false, movementCost = 2)
