42c882d6249ac36a80896efc4ed5a0d540d5556e
plenglin <maximumpwnguin@gmail.com>
2017-11-05 09:50:54 -0800

    Minor changes to both terrain and pawns

--- core/src/io/github/plenglin/questofcon/game/grid/Terrain.kt
+++ core/src/io/github/plenglin/questofcon/game/grid/Terrain.kt
@@ -1,9 +1,6 @@
 package io.github.plenglin.questofcon.game.grid
 
-import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.Texture
 import io.github.plenglin.questofcon.TerrainTextures
-import io.github.plenglin.questofcon.Textures
 
 
 data class Terrain(val name: String, val texture: TerrainTextures, val passable: Boolean, val buildable: Boolean, val movementCost: Int = 1) {
@@ -16,19 +13,11 @@ data class Terrain(val name: String, val texture: TerrainTextures, val passable:
 
 object Terrains {
 
-    val lowlands    = Terrain("lowlands", TerrainTextures.SAND, true, true)
+    val sandy       = Terrain("sandy", TerrainTextures.SAND, true, false)
     val grass       = Terrain("grassland", TerrainTextures.GRASS, true, true)
-    val bigHills    = Terrain("big hills", TerrainTextures.BIGHILL, true, true, movementCost = 3)
+    val bigHills    = Terrain("big hills", TerrainTextures.BIGHILL, true, false, movementCost = 3)
     val hills       = Terrain("hills", TerrainTextures.SMALLHILL, true, true, movementCost = 2)
     val water       = Terrain("water", TerrainTextures.WATER, false, false, movementCost = 3)
     val mountains   = Terrain("mountains", TerrainTextures.MOUNTAIN, false, false)
-
-    /*
-    val forest      = Terrain("forest", Color.BROWN, true, false, movementCost = 2)
-    val desert      = Terrain("desert", Color.GOLD, true, true, movementCost = 2)
-    val gravel      = Terrain("gravel", Color.LIGHT_GRAY, true, true, movementCost = 2)
-    val jungle      = Terrain("jungle", Color.BROWN, true, false, movementCost = 3)
-    val swamp       = Terrain("swamp", Color(0f, 0.4f, 0.25f, 1f), true, true, movementCost = 3)
-    */
 
 }
