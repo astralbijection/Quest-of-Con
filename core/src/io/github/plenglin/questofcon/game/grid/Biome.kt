0acdf97fdf132e278b579591526580aa77eb5791
Astrid Yu <astrid@astrid.tech>
2017-11-05 21:15:59 -0800

    Put these biomes in and add a height normalization function

--- core/src/io/github/plenglin/questofcon/game/grid/Biome.kt
+++ core/src/io/github/plenglin/questofcon/game/grid/Biome.kt
@@ -13,11 +13,11 @@ data class Biome(val name: String, val texture: TerrainTextures, val passable: B
 
 object Biomes {
 
-    val sandy       = Biome("sandy", TerrainTextures.SAND, true, false)
+    val beach       = Biome("beach", TerrainTextures.SAND, true, false)
+    val desert       = Biome("desert", TerrainTextures.SAND, true, false)
     val grass       = Biome("grassland", TerrainTextures.GRASS, true, true)
-    val bigHills    = Biome("big hills", TerrainTextures.BIGHILL, true, false, movementCost = 3)
-    val hills       = Biome("hills", TerrainTextures.SMALLHILL, true, true, movementCost = 2)
-    val water       = Biome("water", TerrainTextures.WATER, false, false, movementCost = 3)
+    val highlands   = Biome("highlands", TerrainTextures.BIGHILL, true, false, movementCost = 2)
+    val water       = Biome("water", TerrainTextures.WATER, false, false)
     val mountains   = Biome("mountains", TerrainTextures.MOUNTAIN, false, false)
 
 }
