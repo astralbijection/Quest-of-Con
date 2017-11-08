1ef7afd740e1156d2fec49acdda8da8138f27ac6
plenglin <maximumpwnguin@gmail.com>
2017-11-07 23:21:44 -0800

    Button to connect

--- core/src/io/github/plenglin/questofcon/screen/MPGameScreen.kt
+++ core/src/io/github/plenglin/questofcon/screen/MPGameScreen.kt
@@ -1,11 +1,18 @@
 package io.github.plenglin.questofcon.screen
 
+import io.github.plenglin.questofcon.net.Client
 import ktx.app.KtxScreen
 
 /**
  *
  */
 object MPGameScreen : KtxScreen {
+
+    lateinit var client: Client
+
+    fun initializeWith(client: Client) {
+        this.client = client
+    }
 
     override fun show() {
 
