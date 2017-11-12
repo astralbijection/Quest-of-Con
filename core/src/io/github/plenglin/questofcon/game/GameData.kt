65cadff87ca98d90b4a2df2796243ba1830be5bb
Astrid Yu <astrid@astrid.tech>
2017-11-12 11:44:45 -0800

    Registry for biomes

--- core/src/io/github/plenglin/questofcon/game/GameData.kt
+++ core/src/io/github/plenglin/questofcon/game/GameData.kt
@@ -2,10 +2,12 @@ package io.github.plenglin.questofcon.game
 
 import io.github.plenglin.questofcon.Assets
 import io.github.plenglin.questofcon.ObjectRegistry
+import io.github.plenglin.questofcon.TerrainTextures
 import io.github.plenglin.questofcon.game.building.BuildingCreator
 import io.github.plenglin.questofcon.game.building.BuildingFactory
 import io.github.plenglin.questofcon.game.building.BuildingHQ
 import io.github.plenglin.questofcon.game.building.BuildingMine
+import io.github.plenglin.questofcon.game.grid.Biome
 import io.github.plenglin.questofcon.game.pawn.PawnArtillery
 import io.github.plenglin.questofcon.game.pawn.PawnCreator
 import io.github.plenglin.questofcon.game.pawn.PawnKnight
@@ -16,7 +18,7 @@ object GameData {
 
     val pawns = ObjectRegistry<PawnCreator>()
     val buildings = ObjectRegistry<BuildingCreator>()
-    val biomes = ObjectRegistry<PawnCreator>()
+    val biomes = ObjectRegistry<Biome>()
 
     val grunt = SimplePawnCreator("inf-grunt", "grunt", 100).apply {
         maxHealth = 30
@@ -60,7 +62,14 @@ object GameData {
         texture = { Assets[Assets.scout] }
     }
 
-    init {
+    val beach       = Biome("biome-beach", "beach", TerrainTextures.SAND, true, false)
+    val desert      = Biome("biome-desert", "desert", TerrainTextures.SAND, true, false)
+    val grass       = Biome("biome-grassland", "grassland", TerrainTextures.GRASS, true, true)
+    val highlands   = Biome("biome-highlands", "highlands", TerrainTextures.BIGHILL, true, false, movementCost = 2)
+    val water       = Biome("biome-water", "water", TerrainTextures.WATER, false, false)
+    val mountains   = Biome("biome-mountains", "mountains", TerrainTextures.MOUNTAIN, false, false)
+
+    fun register() {
         pawns.register(grunt)
         pawns.register(drill)
         pawns.register(beam)
@@ -73,6 +82,13 @@ object GameData {
         buildings.register(BuildingHQ)
         buildings.register(BuildingFactory)
         buildings.register(BuildingMine)
+
+        biomes.register(beach)
+        biomes.register(desert)
+        biomes.register(grass)
+        biomes.register(highlands)
+        biomes.register(water)
+        biomes.register(mountains)
     }
 
 }
